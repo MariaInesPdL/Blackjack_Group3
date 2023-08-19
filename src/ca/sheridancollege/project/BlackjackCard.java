ad80d1c93ba93a4edf8d6b48e924773ff94eb278
Maria Ines Ponce de Leon <poncedel@sheridancollege.ca>
2023-08-18 23:24:09 -0400

    Added comments

--- src/ca/sheridancollege/project/BlackjackCard.java
+++ src/ca/sheridancollege/project/BlackjackCard.java
@@ -5,18 +5,23 @@
 package ca.sheridancollege.project;
 
 /**
- *
+ * Represents a card used in a Blackjack game.
+ * This class extends the Card class and provides methods to get the card's rank, suit,
+ * display the card as a string, and calculate its value in the context of the game.
+ * 
  * @author m_pdl
  */
 public class BlackjackCard extends Card {
     private Rank rank;
     private Suit suit;
 
+    //Constructs a BlackjackCard with the specified suit and rank.
     public BlackjackCard(Suit suit, Rank rank) {
         this.rank = rank;
         this.suit = suit;
     }
 
+    //Retrieves the rank of the card.
     public Rank getRank() {
         return rank;
     }
@@ -26,6 +31,7 @@ public class BlackjackCard extends Card {
         return rank + " of " + suit;
     }
 
+    //Calculates the value of the card
     public int getValue() {
         if (rank == Rank.ACE) {
             return 11; // Can be adjusted during hand calculations
